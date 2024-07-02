f4c3a5081e740200026b422780ebf8fca1b7655a
RoySCV <roys@codevalue.com>
2024-07-02 15:23:32 +0300

    adding comments to main.tf

--- Terraform-EKS-argocd/EKS/main.tf
+++ Terraform-EKS-argocd/EKS/main.tf
@@ -1,107 +1,113 @@
-# https://developer.hashicorp.com/terraform/tutorials/kubernetes/eks
-# https://app.terraform.io/app/eks-test123/settings/varsets
+# This Terraform configuration sets up an EKS cluster on AWS using various modules.
+
+# Provider configuration for AWS
 provider "aws" {
-  region = var.region
-  access_key = ""
-  secret_key = ""
+  region     = var.region      # AWS region to use
+  access_key = ""              # AWS access key (leave empty for security reasons)
+  secret_key = ""              # AWS secret key (leave empty for security reasons)
 }
 
+# Local variables
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}" # Dynamically generates a unique cluster name
+  cluster_name = "education-eks-${random_string.suffix.result}" # Generates a unique cluster name using a random string
 }
 
+# Generate a random string to be used as a suffix
 resource "random_string" "suffix" {
-  length  = 8
-  special = false # Ensures that the generated string is alphanumeric
+  length  = 8      # Length of the random string
+  special = false  # Ensures the string is alphanumeric
 }
 
-# Create a VPC for the EKS cluster using a single AZ
+# Create a VPC for the EKS cluster using multiple availability zones
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
-  name = "multi-az-vpc-eks"
-  cidr = "10.0.0.0/16"
-  azs             = [format("%sa", var.region), format("%sb", var.region)] # Two AZs for high availability
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+  source  = "terraform-aws-modules/vpc/aws"  # Source of the VPC module
+  version = "5.0.0"                          # Version of the VPC module
 
-  enable_nat_gateway = true
-  enable_dns_hostnames = true
+  name = "multi-az-vpc-eks"  # Name of the VPC
+  cidr = "10.0.0.0/16"       # CIDR block for the VPC
+
+  azs             = [format("%sa", var.region), format("%sb", var.region)] # Availability zones for high availability
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]                         # Private subnets
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]                         # Public subnets
+
+  enable_nat_gateway = true  # Enable NAT Gateway
+  enable_dns_hostnames = true  # Enable DNS hostnames
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared" # Tag for Kubernetes cluster
+    "kubernetes.io/role/elb"                      = 1        # Tag for external load balancer
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"  # Tag for Kubernetes cluster
+    "kubernetes.io/role/internal-elb"             = 1         # Tag for internal load balancer
   }
 }
 
-# EKS Cluster configuration using a single private subnet
+# EKS Cluster configuration
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3"
-  cluster_name    = local.cluster_name
-  cluster_version = "1.27"
+  source  = "terraform-aws-modules/eks/aws"  # Source of the EKS module
+  version = "19.15.3"                        # Version of the EKS module
 
-  vpc_id      = module.vpc.vpc_id
-  subnet_ids  = module.vpc.private_subnets # Use the private subnets for node groups
-  cluster_endpoint_public_access = true # Allows public access to the Kubernetes API server
-  # Default configurations applied to all node groups
+  cluster_name    = local.cluster_name       # Name of the EKS cluster
+  cluster_version = "1.27"                   # Kubernetes version
+
+  vpc_id     = module.vpc.vpc_id             # VPC ID
+  subnet_ids = module.vpc.private_subnets    # Subnet IDs
+
+  cluster_endpoint_public_access = true  # Allows public access to the Kubernetes API server
+
+  # Default configurations for node groups
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64" # Specifies the Amazon Linux 2 AMI type for the nodes
-
+    ami_type = "AL2_x86_64"  # Amazon Linux 2 AMI type for the nodes
   }
 
+  # Configuration for managed node groups
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
-      instance_types = ["t3.small"]
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      name          = "node-group-1"  # Name of the first node group
+      instance_types = ["t3.small"]   # Instance types for the nodes
+      min_size      = 1               # Minimum size of the node group
+      max_size      = 3               # Maximum size of the node group
+      desired_size  = 2               # Desired size of the node group
     }
 
     two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      name          = "node-group-2"  # Name of the second node group
+      instance_types = ["t3.small"]   # Instance types for the nodes
+      min_size      = 1               # Minimum size of the node group
+      max_size      = 2               # Maximum size of the node group
+      desired_size  = 1               # Desired size of the node group
     }
   }
 }
 
-
-# Retrieves the AWS IAM policy for the EBS CSI driver
+# Data source to retrieve the AWS IAM policy for the EBS CSI driver
 data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"  # ARN of the EBS CSI driver policy
 }
 
-# IAM role for the EBS CSI driver using OIDC
+# IAM role for the EBS CSI driver using OpenID Connect (OIDC)
 module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "4.7.0"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"  # Source of the IAM module
+  version = "4.7.0"                                                                # Version of the IAM module
 
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+  create_role                   = true                                          # Flag to create the role
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"  # Name of the IAM role
+  provider_url                  = module.eks.oidc_provider                      # OIDC provider URL
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]      # ARN of the policy to attach to the role
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]  # Subjects for the OIDC role
 }
 
 # EBS CSI driver EKS add-on
 resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.20.0-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+  cluster_name             = module.eks.cluster_name  # Name of the EKS cluster
+  addon_name               = "aws-ebs-csi-driver"     # Name of the add-on
+  addon_version            = "v1.20.0-eksbuild.1"     # Version of the add-on
+  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn  # IAM role ARN for the service account
+
   tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
+    "eks_addon" = "ebs-csi"  # Tag for the add-on
+    "terraform" = "true"     # Tag for Terraform
   }
 }
